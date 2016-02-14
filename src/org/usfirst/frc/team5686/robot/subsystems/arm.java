fd6f7eceba2865797852c9814c1a874673e1a165
gofightwin87 <marioncarr99@gmail.com>
2016-02-14 16:54:14 -0500

    polishing

--- src/org/usfirst/frc/team5686/robot/subsystems/arm.java
+++ src/org/usfirst/frc/team5686/robot/subsystems/arm.java
@@ -1,18 +1,17 @@
 package org.usfirst.frc.team5686.robot.subsystems;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.SpeedController;
 import org.usfirst.frc.team5686.robot.Ports;
 import org.usfirst.frc.team5686.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.Counter;
 public class arm extends Subsystem {
 	public static arm instance;
 	public static arm arm1 = RobotMap.ARM;
 	static Talon motor = RobotMap.DRIVE_TRAIN_ARM_MOTOR ;
-
+	static DigitalInput limitSwitch = new DigitalInput(1);
+	static Counter counter = new Counter (limitSwitch);
 	int motorSpeedUp = Ports.armMotorSpeedUp;
 	int motorSpeedDown = Ports.armMotorSpeedDown;
 	int motorStop = Ports.motorStop;
@@ -29,11 +28,11 @@ public class arm extends Subsystem {
 	// here. Call these from Commands.
 
 	public static boolean isSwitchSet() {
-		return Counter.get() > 0;
+		return counter.get() > 0;
 	}
 
 	public void initializeCounter() {
-		Counter.reset();
+		counter.reset();
 	}
 	public void initDefaultCommand() {
 		instance = this;
